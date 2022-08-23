14585335b9bf61af970e3f4bd9ffecb34d41e49a
Saransh <saranshgupta.saru@gmail.com>
2022-08-24 00:51:23 +0530

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,58 @@
+const express  = require('express');
+const User = require('../models/user');
+const router= express.Router();
+const passport = require('passport');
+const flash = require('connect-flash');
+
+//for signup page/form
+router.get('/register', (req,res)=>{
+    res.render('auth/signup',{message: req.flash('error')});
+})
+
+
+//routes to register
+router.post('/register', async(req,res)=>{
+
+    try{
+        // console.log(req.body);
+    const user = {
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    email: req.body.email,
+    username: req.body.username
+}
+const newUser = await User.register(user,req.body.password);
+res.send(newUser);
+    }
+
+    catch(e){
+        req.flash('error', e.message)
+        res.redirect('/register');
+    }
+
+})
+
+
+//for login page
+router.get('/login', (req,res)=>{
+    res.render('auth/login');
+})
+
+//verify the user to be loggedin
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }), (req,res)=>{
+        res.redirect('home');
+  
+  });
+
+  //logout(this function accepts a callback)
+  router.get('/logout',(req,res)=>{
+    req.logout(()=>{
+        res.redirect('/login');
+    });
+});
+
+
+module.exports = router;
